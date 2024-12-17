e67462af4915820e8e35c27a671ee5eb4253c5b5
JohnnyWu <orion_wu@yahoo.com.tw>
2024-12-17 11:51:31 +0800

    前面package雖然開好, 但是裡面沒有放檔案, git不會記錄 這個版本在每個package放進Home的三個檔案作為sample, 再請pull下來後自行修改

---
+++ app/src/main/java/com/example/potel/ui/discusszone/HomeViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.potel.ui.discusszone
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * todo 2-3 宣告 HomeViewModel
+ * 討論：
+ * (1) 為什麼要 ViewModel
+ * (2) ViewModel 要做什麼事情，有哪些資料應該放到 ViewModel
+ * */
+class HomeViewModel : ViewModel() {
+
+//    private val _items = MutableStateFlow<List<TipHomeItemUiState>>(listOf())
+//    val items = _items.asStateFlow()
+
+
+    fun getApiData() {
+//        // todo 2-5 取得 API 資料，目前先用假資料
+//        _items.update {
+//            listOf(
+//                TipHomeItemUiState(
+//                    title = "Home",
+//                    imageVector = Icons.Filled.Home
+//                ),
+//                TipHomeItemUiState(
+//                    title = "Search",
+//                    imageVector = Icons.Filled.Search
+//                ),
+//                TipHomeItemUiState(
+//                    title = "Delete",
+//                    imageVector = Icons.Filled.Delete
+//                ),
+//            )
+//        }
+    }
+
+
+}
