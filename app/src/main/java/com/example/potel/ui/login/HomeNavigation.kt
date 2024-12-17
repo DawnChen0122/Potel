84f5987001c09a360ca00b90ec0727c2998e16bb
Jim <a86421@gmail.com>
2024-12-17 15:23:55 +0800

    no message

---
+++ app/src/main/java/com/example/potel/ui/login/HomeNavigation.kt
@@ -0,0 +1,21 @@
+package com.example.potel.ui.login
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+
+/**
+ * todo 2-1 將首頁的路由獨立出來
+ * */
+
+const val HOME_NAVIGATION_ROUTE = "home"
+
+fun genHomeNavigationRoute() = HOME_NAVIGATION_ROUTE
+
+fun NavGraphBuilder.homeScreenRoute(navController: NavHostController) {
+    composable(
+        route = HOME_NAVIGATION_ROUTE,
+    ) {
+        HomeRoute(navController = navController)
+    }
+}
