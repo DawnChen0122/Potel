d052117c83c33d367f95120ac8f512e14c1a4bc4
Jim <a86421@gmail.com>
2024-12-23 00:32:30 +0800

    no message

--- app/src/main/java/com/example/potel/ui/home/AccountNavigation.kt
+++ app/src/main/java/com/example/potel/ui/home/AccountNavigation.kt
@@ -1,21 +1,20 @@
-//package com.example.potel.ui.home
-//
-//import androidx.navigation.NavGraphBuilder
-//import androidx.navigation.NavHostController
-//import androidx.navigation.compose.composable
-//
-///**
-// * todo 2-1 將首頁的路由獨立出來
-// * */
-//
-//const val HOME_NAVIGATION_ROUTE = "home"
-//
-//fun genHomeNavigationRoute() = HOME_NAVIGATION_ROUTE
-//
-//fun NavGraphBuilder.homeScreenRoute(navController: NavHostController) {
-//    composable(
-//        route = HOME_NAVIGATION_ROUTE,
-//    ) {
-//        HomeRoute(navController = navController)
-//    }
-//}
+package com.example.potel.ui.home
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.potel.ui.home.*
+
+enum class Screens(title: String){
+    LogIn(title = "登入")
+}
+
+fun NavGraphBuilder.homeScreen1Route(navController: NavHostController) {
+    composable(
+        route = Screens.LogIn.name,
+    ) {
+        Login(
+            navController = navController
+        )
+    }
+}
