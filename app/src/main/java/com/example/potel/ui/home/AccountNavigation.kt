399db98cbcdc17ac0f29549542b38fa33628c952
Jim <a86421@gmail.com>
2024-12-23 09:19:28 +0800

    no message

--- app/src/main/java/com/example/potel/ui/home/AccountNavigation.kt
+++ app/src/main/java/com/example/potel/ui/home/AccountNavigation.kt
@@ -3,17 +3,16 @@ package com.example.potel.ui.home
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import com.example.potel.ui.home.*
 
-enum class Screens(title: String){
-    LogIn(title = "登入")
+enum class LogIn (title: String){
+    LogIn(title = "登入畫面")
 }
 
 fun NavGraphBuilder.homeScreen1Route(navController: NavHostController) {
     composable(
-        route = Screens.LogIn.name,
+        route = Screens.MOS01.name,
     ) {
-        Login(
+        ScreenMOS01(
             navController = navController
         )
     }
