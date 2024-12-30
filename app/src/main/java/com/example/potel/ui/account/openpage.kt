d368cad0bc54deac421543aaf3d03b2feeef1c60
Jim <a86421@gmail.com>
2024-12-30 17:49:51 +0800

    設定登入頁面

--- app/src/main/java/com/example/potel/ui/account/openpage.kt
+++ app/src/main/java/com/example/potel/ui/account/openpage.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -19,21 +20,21 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.potel.ui.home.HOME_NAVIGATION_ROUTE
 
 @Composable
 fun Openpage(navController: NavHostController) {
 
-    val email = remember { mutableStateOf("") }
-    var emailError by remember { mutableStateOf(false) }
+    val input = remember { mutableStateOf("") }
+    var inputError by remember { mutableStateOf(false) }
+
     val emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$".toRegex()
 
-    val phonenumber = remember { mutableStateOf("") }
-    var phonenumberError by remember { mutableStateOf(false) }
+    val phoneRegex = "^[0-9]{10}$".toRegex()
 
     val password = remember { mutableStateOf("") }
     var passwordVisible by remember { mutableStateOf(false) }
     var passwordError by remember { mutableStateOf(false) }
-
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -42,46 +43,34 @@ fun Openpage(navController: NavHostController) {
             .padding(10.dp)
     ) {
 
-        OutlinedTextField(
-            value = email.value,
-            onValueChange = {
-                email.value = it
-                emailError = !it.matches(emailRegex)
+        Text(
+            modifier = Modifier.clickable{
+                navController.navigate(HOME_NAVIGATION_ROUTE)
             },
-            label = { Text(text = "請輸入信箱") },
+            text = "Potel",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue
+        )
+
+        OutlinedTextField(
+            value = input.value,
+            onValueChange = {
+                input.value = it
+                inputError = !(it.matches(emailRegex) || it.matches(phoneRegex))
+            },
+            label = { Text(text = "請輸入信箱或手機號碼") },
             singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
             shape = RoundedCornerShape(8.dp),
-            isError = emailError,
+            isError = inputError,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 10.dp)
         )
-        if (emailError) {
+        if (inputError) {
             Text(
-                text = "請輸入有效的信箱",
-                color = Color.Red,
-                fontSize = 12.sp,
-                modifier = Modifier.padding(start = 16.dp)
-            )
-        }
-
-        OutlinedTextField(
-            value = phonenumber.value,
-            onValueChange = { phonenumber.value = it
-                phonenumberError = it.isEmpty() || it.length != 10 },
-            label = { Text(text = "請輸入手機號碼") },
-            singleLine = true,
-            shape = RoundedCornerShape(8.dp),
-            isError = phonenumberError,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        )
-        if (phonenumberError) {
-            Text(
-                text = "手機號碼為十位數字",
+                text = "請輸入有效的信箱或手機號碼",
                 color = Color.Red,
                 fontSize = 12.sp,
                 modifier = Modifier.padding(start = 16.dp)
@@ -92,7 +81,8 @@ fun Openpage(navController: NavHostController) {
             value = password.value,
             onValueChange = {
                 password.value = it
-                passwordError = it.isEmpty() || it.length < 6 || it.length > 20 },
+                passwordError = it.isEmpty() || it.length < 6 || it.length > 20
+            },
             label = { Text(text = "密碼") },
             leadingIcon = {
                 Icon(
@@ -131,15 +121,19 @@ fun Openpage(navController: NavHostController) {
             )
         }
 
-
         Button(
             onClick = {
-                if ( email.value.isEmpty() || password.value.isEmpty()) {
+                if (input.value.isEmpty() || password.value.isEmpty()) {
                     // 顯示錯誤提示
                 } else if (password.value != password.value) {
                     // 顯示密碼不一致的錯誤提示
                 } else {
-                    "執行註冊邏輯"
+                    // 執行登入邏輯
+                    if (input.value.matches(emailRegex)) {
+                        // 處理Email登入邏輯
+                    } else if (input.value.matches(phoneRegex)) {
+                        // 處理手機號碼登入邏輯
+                    }
                 }
             },
             modifier = Modifier
@@ -149,10 +143,8 @@ fun Openpage(navController: NavHostController) {
         ) {
             Text(text = "登入", fontSize = 20.sp)
         }
-
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
