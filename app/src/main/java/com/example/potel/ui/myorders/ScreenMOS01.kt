d731d04def96619708a2924af24ea3c8f17ce0e4
Jim <a86421@gmail.com>
2025-01-02 17:15:55 +0800

    no message

--- app/src/main/java/com/example/potel/ui/myorders/ScreenMOS01.kt
+++ app/src/main/java/com/example/potel/ui/myorders/ScreenMOS01.kt
@@ -22,11 +22,14 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.potel.R
+import com.example.potel.ui.theme.PotelTheme
 
 @Composable
 fun ScreenMOS01(
@@ -183,10 +186,10 @@ fun ScreenMOS01(
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun MainPreview() {
-//    PotelTheme {
-//        ScreenMOS01(navController = rememberNavController())
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun ScreenMOS01() {
+    PotelTheme {
+        ScreenMOS01(navController = rememberNavController())
+    }
+}
