4eaf164eb581c06f1cf8381835b71c069c4dffb4
Riley <rileyshih55@gmil.com>
2025-01-12 00:17:52 +0800

    no message

--- app/src/main/java/com/example/potel/ui/booking/ApiService.kt
+++ app/src/main/java/com/example/potel/ui/booking/ApiService.kt
@@ -1,8 +1,5 @@
 package com.example.potel.ui.booking
 
-import com.example.potel.ui.myorders.Order
-import com.example.potel.ui.myorders.PrdOrder
-import com.example.potel.ui.myorders.ResponseObject
 import com.example.potel.ui.myorders.utils.MyCookieJar
 import okhttp3.OkHttpClient
 import retrofit2.Response
@@ -11,23 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Query
-import java.sql.Timestamp
 
-
-data class RoomTypeResponse(
-    val roomTypeId:Int,
-    val descpt:String,
-    val imageId:Int,
-    val price:Int,
-    val petType:Char,
-    val weightL:Int,
-    val weightH:Int,
-    val status:Char,
-    val createDate:Timestamp,
-    val modifyDate:Timestamp
-)
 
 interface ApiService {
 
