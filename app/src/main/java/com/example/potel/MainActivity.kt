188dfe158f59af0daa9f505f1f0eb27d11a09190
Jim <a86421@gmail.com>
2025-01-12 11:04:55 +0800

    登入 註冊 忘記密碼 功能完成

--- app/src/main/java/com/example/potel/MainActivity.kt
+++ app/src/main/java/com/example/potel/MainActivity.kt
@@ -134,7 +134,7 @@ fun TipNavHost(
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = AccountScreens.Signup.name
+        startDestination = AccountScreens.Login.name
 
     ) {
         // todo 2-2 置入所有的畫面路徑
